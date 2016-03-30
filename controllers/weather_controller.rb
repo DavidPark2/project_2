983d3713fcaeebe23d9f57311be607febff2f578
Armando Robledo <armando.robledo@gmail.com>
2016-03-30 10:07:04 -0500

    background

--- controllers/weather_controller.rb
+++ controllers/weather_controller.rb
@@ -5,18 +5,8 @@ class WeatherController < ApplicationController
   end
 
   post '/' do
-    @weather = Account.create username: params[:username], email: params[:email],  password: password
+    @weather = Account.create username: params[:username], email: params[:email], password: password
     @weathers = Weather.all
     erb :weather
   end
 end
-
-
-# We will use this in order to get long/lad from google
-# require 'rest-client'
-#
-# location = RestClient.get 'your url here'
-#
-# coordinates = JSON.parse(location.body)
-#
-# coordinates['latitude']
