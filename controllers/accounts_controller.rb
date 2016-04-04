51e9361f7eae209250bd4595d6d54155c1432cf9
David Park <dpark543107@gmail.com>
2016-04-04 16:34:28 -0500

    cleaned up code and added log out button

--- controllers/accounts_controller.rb
+++ controllers/accounts_controller.rb
@@ -6,38 +6,29 @@ class AccountController < ApplicationController
 
   post '/create' do
     password = BCrypt::Password.create(params[:password])
-
     @account = Account.create username: params[:username], email: params[:email],  password: password
-
     session[:logged_in] = true
     session[:username] = params[:username]
     session[:email] = params[:email]
     session[:password] = password
     session[:user_id] = @account.id
-
     redirect '/weather'
   end
 
   get '/create' do
     redirect '/' if !session[:logged_in]
-
     accounts = Account.all
-
     sessionName = "Hello #{session[:username]}! #{session[:email]} #{session[:password]} Welcome back!"
-
   end
 
   post '/login' do
-
     account = Account.where(email: params[:username]).first
-
     compare_to = BCrypt::Password.new(account.password)
     if compare_to == params[:password]
       session[:logged_in] = true
       session[:email] = params[:username]
       # session[:email] = account.email
       session[:user_id] = account.id
-
       redirect '/weather'
       # "Welcome back #{session[:email]}!"
     else
@@ -47,5 +38,10 @@ class AccountController < ApplicationController
 
   get '/login' do
     session[:logged_in] = false
+  end
+
+  get '/logout' do
+    session[:logged_in] = false
+    redirect '/accounts'
   end
 end
